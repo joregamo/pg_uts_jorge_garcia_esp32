32f7608035f85c845d2b1c73de386e8ed4432233
Jorge E Garcia Moreno <joregamodv@gmail.com>
2023-10-26 01:57:52 -0500

    Se creó la función del sensor de corriente ACS712 y se calibró midiendo el voltaje offset ajustando el offset en el codigo

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,37 @@
 #include <Arduino.h>
 #include "VoltageSensor.h"
+#include "CurrentSensor.h"
 
-
-
-//Sensor Voltaje FZ043 para medir el panel.
-
+// Sensor de voltaje
 const int voltageSensorPin = 32;
-VoltageSensor sensor(voltageSensorPin);
+VoltageSensor voltageSensor(voltageSensorPin);
+
+// Sensor de corriente
+const int currentSensorPin = 27;
+CurrentSensor currentSensor(currentSensorPin);
 
 void setup() {
     Serial.begin(115200);
 }
 
 void loop() {
-    float voltage = sensor.readVoltage();
-    float voltageReal = voltage/0.1; // 0.13 factor de conversion
+    // Lectura y despliegue del voltaje
+    float voltage = voltageSensor.readVoltage();
+    float voltageReal = voltage / 0.1; // Factor de conversión para el voltaje
   
     Serial.print("Voltage: ");
     Serial.print(voltageReal, 2);
     Serial.println(" V");
 
+    // Lectura y despliegue de la corriente
+    float current = currentSensor.getCurrent();
+  
+    Serial.print("Current: ");
+    Serial.print(current, 2);
+    Serial.println(" A");
+
     delay(1000);
 }
+
 
 
