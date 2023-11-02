0aff8f7e265d3a17b5353626a65311086de11f06
Jorge E Garcia Moreno <joregamodv@gmail.com>
2023-11-01 19:02:20 -0500

    con 10mA de diferencia con respecto a la lectura real se ajustó la función del sensor  corriente otra vez

--- lib/CurrentSensor/CurrentSensor.h
+++ lib/CurrentSensor/CurrentSensor.h
@@ -10,7 +10,7 @@ private:
     int _pin;
     static constexpr float ADC_RESOLUTION = 4095.0; // ESP32 ADC is 12-bit
     static constexpr float V_REF = 3.3; // Reference voltage for ESP32 ADC
-    static constexpr float SENSOR_OFFSET = 0.6167; // 0.617 a 0.98V for the ACS712 when no current
+    static constexpr float SENSOR_OFFSET = 0.6167; // 0.617 a 0.98V for the ACS712 when no current (con 10mA de diferencia con respecto a la lectura real)
     static constexpr float SENSOR_SENSITIVITY = 0.1; // Sensitivity of the ACS712 20A module (100 mV/A)
 };
 
